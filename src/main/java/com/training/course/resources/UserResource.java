643bd10b2e57151de6fbb191f03db927ff49028f
Djalma Xavier <djalmaxavier42@gmail.com>
2024-12-26 14:19:49 -0300

    User entity and Resource

---
+++ src/main/java/com/training/course/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.training.course.resources;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.training.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Djalma", "djalma@gmail", "23423432", "teste");
+		return ResponseEntity.ok().body(u);
+	}
+}
